8f8f5fa15563a1907f3bbcc9564c934a5deb647e
kelvincedcas <kelvincedcas@gmail.com>
2026-02-01 12:51:37 -0500

    fixed

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -30,8 +30,8 @@ export const MobileHeader = () => {
       <header
         className="
         fixed top-0 left-0 right-0 z-50
-        bg-background/70
-        backdrop-blur-xl
+        bg-background/60
+        backdrop-blur-xl  
         border-b border-border/50
         py-3
       "
